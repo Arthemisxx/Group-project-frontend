f2588861d848f8ac50e0d0a926967afc95ce1f64
Julia Staniszewska <staniszewska.julia@wp.pl>
2025-10-31 19:45:53 +0100

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { useState } from 'react'
-import smileyFace from './assets/smiley-face.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://www.geeksforgeeks.org/reactjs/react/" target="_blank">
-          <img src={smileyFace} className="logo react" alt="Smiley Face" />
-        </a>
-      </div>
-      <h1>Tu będzie na pewno bardzo ładna strona</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+
     </>
   )
 }
