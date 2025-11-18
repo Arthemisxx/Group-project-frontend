b4c91b8b1011ccf75bd73d77950ced63705c6205
Ewa <glowacka.ewa@hotmail.com>
2025-11-18 19:15:16 +0100

    First header update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import './App.css'
 import {Map} from "./Map/Map.tsx"
+import Header from "./Header/Header";
+
 
 function App() {
 
   return (
     <>
       <Map/>
+      <Header />
 
     </>
   )
 }
+
+
 
 export default App
