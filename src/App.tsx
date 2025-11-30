8e84789d97005c0d7836e4071d6b15abf5a3fb39
Julia Staniszewska <staniszewska.julia@wp.pl>
2025-11-30 19:44:11 +0100

    Routing fix 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import './App.css'
 import Header from "./Header/Header";
 import Home from "./Home/Home.tsx";
-import Odkrywaj from "./Odkrywaj/Odkrywaj.tsx";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {MapView} from "./Map/MapView.tsx";
+import {Explore} from "./Explore/Explore.tsx";
 
 function App() {
-
   return (
     <>
       <BrowserRouter>
@@ -14,14 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/mapa" element={<MapView />} />
-        <Route path="/odkrywaj" element={<Odkrywaj />} />
+        <Route path="/odkrywaj" element={<Explore />} />
       </Routes>
       </BrowserRouter>
-
     </>
   )
 }
-
-
 
 export default App
--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Body } from "../Body/Body";
+import {Explore} from "../Explore/Explore.tsx";
 
 export default function Home() {
   return (
     <div>
       <Body />
+        <Explore/>
       {/* Galeria*/}
     </div>
   );
